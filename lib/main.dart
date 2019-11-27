453d1b344df4ea17d31f688ba4b613f1452abf3c
marco <marco.dirosa8@gmail.com>
2019-11-27 09:15:25 +0000

    hello_world

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:functional_widget_annotation/functional_widget_annotation.dart';
+
+part 'main.g.dart';
 
 void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: App(),
-      ),
-    );
-  }
+@FunctionalWidget(widgetType: FunctionalWidgetType.stateless)
+Widget myApp() {
+  return MaterialApp(
+    home: Scaffold(
+      body: App(),
+    ),
+  );
 }
 
-class App extends StatefulWidget {
-  @override
-  AppState createState() => AppState();
-}
-
-class AppState extends State<App> {
-  Color caughtColor = Colors.grey;
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        DragBox(Offset(0.0, 0.0), 'Box One', Colors.blueAccent),
-        DragBox(Offset(200.0, 0.0), 'Box Two', Colors.orange),
-        DragBox(Offset(300.0, 0.0), 'Box Three', Colors.lightGreen),
-        Positioned(
-          left: 100.0,
-          bottom: 0.0,
-          child: DragTarget(
-            onAccept: (Color color) {
-              caughtColor = color;
-            },
-            builder: (
-              BuildContext context,
-              List<dynamic> accepted,
-              List<dynamic> rejected,
-            ) {
-              return Container(
-                width: 200.0,
-                height: 200.0,
-                decoration: BoxDecoration(
-                  color: accepted.isEmpty ? caughtColor : Colors.grey.shade200,
-                ),
-                child: Center(
-                  child: Text("Drag Here!"),
-                ),
-              );
-            },
-          ),
-        )
-      ],
-    );
-  }
-}
-
-class DragBox extends StatefulWidget {
-  final Offset initPos;
-  final String label;
-  final Color itemColor;
-
-  DragBox(this.initPos, this.label, this.itemColor);
-
-  @override
-  DragBoxState createState() => DragBoxState();
-}
-
-class DragBoxState extends State<DragBox> {
-  Offset position = Offset(0.0, 0.0);
-
-  @override
-  void initState() {
-    super.initState();
-    position = widget.initPos;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Positioned(
-        left: position.dx,
-        top: position.dy,
-        child: Draggable(
-          data: widget.itemColor,
-          child: Container(
-            width: 100.0,
-            height: 100.0,
-            color: widget.itemColor,
-            child: Center(
-              child: Text(
-                widget.label,
-                style: TextStyle(
-                  color: Colors.white,
-                  decoration: TextDecoration.none,
-                  fontSize: 20.0,
-                ),
-              ),
-            ),
-          ),
-          onDraggableCanceled: (velocity, offset) {
-            setState(() {
-              position = offset;
-            });
+@hwidget
+Widget app() {
+  final caughtColor = useState(Colors.white);
+  return Stack(
+    children: <Widget>[
+      DragBox(Offset(0.0, 0.0), 'Box one', Colors.red),
+      DragBox(Offset(150.0, 0.0), 'Box two', Colors.cyan),
+      DragBox(Offset(300.0, 15.0), 'Box three', Colors.amber),
+      Positioned(
+        left: 100.0,
+        bottom: 0.0,
+        child: DragTarget(
+          onAccept: (Color color) {
+            caughtColor.value = color;
           },
-          feedback: Container(
-            width: 120.0,
-            height: 120.0,
-            color: widget.itemColor.withOpacity(0.5),
-            child: Center(
-              child: Text(
-                widget.label,
-                style: TextStyle(
-                  color: Colors.white,
-                  decoration: TextDecoration.none,
-                  fontSize: 18.0,
-                ),
+          builder: (context, accepted, rejected) {
+            return Container(
+              width: 200.0,
+              height: 200.0,
+              decoration: BoxDecoration(
+                  color: accepted.isEmpty ? caughtColor.value : Colors.grey
+                      .shade400
               ),
-            ),
+              child: Center(
+                child: Text("Drag here"),
+              ),
+            );
+          },
+        ),
+      ),
+    ],
+  );
+}
+
+@hwidget
+Widget dragBox(Offset initPos, String label, Color itemColor) {
+  final position = useState(initPos);
+  return Positioned(
+    left: position.value.dx,
+    top: position.value.dy,
+    child: Draggable(
+      data: itemColor,
+      child: Container(
+        width: 100,
+        height: 100,
+        color: itemColor,
+        child: Center(
+          child: Text(
+              label
           ),
-        ));
-  }
+        ),
+      ),
+      onDraggableCanceled: (velocity, offset) {
+        position.value = offset;
+      },
+      feedback: Container(
+        width: 120,
+        height: 120,
+        color: itemColor.withOpacity(0.5),
+        child: Center(
+          child: Text(label),
+        ),
+      ),
+    ),
+  );
 }
